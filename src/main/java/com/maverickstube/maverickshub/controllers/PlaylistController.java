01f2d8467b88d9aebacc7ee9ec18e22776e1d2a9
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-06-16 11:32:17 +0100

    commit

--- src/main/java/com/maverickstube/maverickshub/controllers/PlaylistController.java
+++ src/main/java/com/maverickstube/maverickshub/controllers/PlaylistController.java
@@ -25,7 +25,6 @@ public class PlaylistController {
     @PostMapping
     public ResponseEntity<?> createPlaylist(@RequestBody CreatePlaylistRequest request) throws UserNotFoundException {
         log.info("Creating playlist: {}", request);
-        System.out.println("de");
         CreatePlaylistResponse response = playlistService.create(request);
         log.info("Created playlist: {}", response);
         return ResponseEntity.status(CREATED).body(response);
